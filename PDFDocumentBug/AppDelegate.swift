6bb7a62475973b49c032fc9c7cac92414ffc4126
Axel <soulchildsince2013@gmail.com>
2018-09-05 23:14:37 +0800

    Initial Commit

---
+++ PDFDocumentBug/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PDFDocumentBug
+//
+//  Created by Soulchild on 05/09/2018.
+//  Copyright © 2018 soulchild. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PDFDocumentBug/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PDFDocumentBug
+//
+//  Created by Soulchild on 05/09/2018.
+//  Copyright © 2018 soulchild. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
